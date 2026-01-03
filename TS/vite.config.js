36fd7679e58c8ae5a424c600d784ce2f8deb0b8f
jagadeeshk189 <jagadeeshk189@gmail.com>
2026-01-03 18:10:45 +0530

    Add vite dev dependency (fix vercel build)

--- TS/vite.config.js
+++ TS/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
         },
+    },
+    server: {
+        host: true, // 👈 allow LAN access
+        port: 5173,
+        strictPort: true,
     },
     css: {
         devSourcemap: false,
