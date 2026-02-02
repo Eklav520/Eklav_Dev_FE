8122608a49f5d54a65fc006a4f3085c32eb1bb30
jagadeeshk189 <jagadeeshk189@gmail.com>
2026-02-02 22:15:45 +0530

    Changes for Admin Jobs

--- TS/src/app/instructor/interview-details/components/AdminJobForm.tsx
+++ TS/src/app/instructor/interview-details/components/AdminJobForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Form, Button, Row, Col, Alert, Spinner } from 'react-bootstrap'
 import ReactQuill from 'react-quill-new'
-import 'react-quill-new/dist/quill.snow.css'
+import 'quill/dist/quill.snow.css'
 
 
 const AdminJobForm: React.FC = () => {
